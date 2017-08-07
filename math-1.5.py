ea9ef3760aca56535b7b86867f376abe9a83b01f
Marcel Zuppinger <marcel.zuppinger@zuro.ch>
2017-08-07 19:13:08 +0800

    Update first 5 sesssions

---
+++ math-1.5.py
@@ -0,0 +1,8 @@
+# To find out the type of a value or a variable that refers to that value,
+# you can use the type() function. Suppose you've defined a variable a,
+# but you forgot the type of this variable. To determine the type of a,
+# simply execute:
+
+a = ("Hallo")
+
+type(a)
